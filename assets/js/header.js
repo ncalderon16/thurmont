0923a183e9bf1f12d4c16ee932d3aa8008dcb158
ncalderon16 <ncalderon1916@gmail.com>
2025-02-16 18:24:47 -0800

    Add permalinks to each page

--- assets/js/header.js
+++ assets/js/header.js
@@ -2,32 +2,32 @@ const template = document.createElement("template");
 template.innerHTML = `
     <div id="header">
         <div id="header-logo">
-            <a href="../../pages/home.html">
+            <a href="/">
                 <img src="../../image/thurmont-logo.jpg" width="110" height="110">
             </a>
         </div>
         
         <div class="header-options">
             <div class="header-option">
-                <a href="../../pages/home.html" target="_self">
+                <a href="/" target="_self">
                     <span class="header-option">Home</span>
                 </a>
             </div>
             
             <div class="header-option">
-                <a href="../../pages/services.html" target="_self">
+                <a href="/services" target="_self">
                     <span class="header-option">Services</span>
                 </a>
             </div>
             
             <div class="header-option">
-                <a href="../../pages/about.html" target="_self">
+                <a href="/about" target="_self">
                     <span class="header-option">About</span>
                 </a>
             </div>
             
             <div class="header-option">
-                <a href="../../pages/partners.html" target="_self">
+                <a href="/partners" target="_self">
                     <span class="header-option">Partners</span>
                 </a>
             </div>
